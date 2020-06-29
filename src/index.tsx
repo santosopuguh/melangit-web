c77c459049b055acfd03a0103cd137880394a099
santosopuguh <tri.puguh@gmail.com>
2020-06-29 20:54:50 +0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import TopNavigation from "./components/TopNavigation";
 
 ReactDOM.render(
   <React.StrictMode>
+    <TopNavigation />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
